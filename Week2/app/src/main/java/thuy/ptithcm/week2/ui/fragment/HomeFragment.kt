876e7b1a319e758597ef8d8d081beecf0ed8749c
Thủy Phạm <32641436+phamthuy1998@users.noreply.github.com>
2020-03-03 09:59:27 +0700

    change dialog bottom sheet

--- Week2/app/src/main/java/thuy/ptithcm/week2/ui/fragment/HomeFragment.kt
+++ Week2/app/src/main/java/thuy/ptithcm/week2/ui/fragment/HomeFragment.kt
@@ -1,14 +1,13 @@
 package thuy.ptithcm.week2.ui.fragment
 
 
-import android.app.Dialog
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
@@ -16,7 +15,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.deishelon.roundedbottomsheet.RoundedBottomSheetDialog
-import kotlinx.android.synthetic.main.dialog_filter.*
+import kotlinx.android.synthetic.main.dialog_filter.view.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import thuy.ptithcm.week2.R
 import thuy.ptithcm.week2.model.Story
@@ -36,7 +35,6 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var storyViewModel: StoryViewModel
-    private var showDialog = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -58,8 +56,7 @@ class HomeFragment : Fragment() {
 
     private fun addEvent() {
         btn_menu_section.setOnClickListener {
-            if (!showDialog)
-                showDialogSection()
+            showDialogSection()
         }
         swipeContainer.setOnRefreshListener { refreshLayout() }
     }
@@ -73,14 +70,15 @@ class HomeFragment : Fragment() {
         )
     }
 
+    @SuppressLint("InflateParams")
     private fun showDialogSection() {
         val mBottomSheetDialog = RoundedBottomSheetDialog(requireContext())
-        val sheetView = layoutInflater.inflate(R.layout.dialog_filter, null)
+        val dialog = layoutInflater.inflate(R.layout.dialog_filter, null)
 
-        val dialog = Dialog(requireContext())
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setCancelable(false)
-        dialog.setContentView(R.layout.dialog_filter)
+//        val dialog = Dialog(requireContext())
+//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        dialog.setCancelable(false)
+//        dialog.setContentView(R.layout.dialog_filter)
         dialog.edt_bgdate.isEnabled = false
         dialog.edt_end_date.isEnabled = false
         dialog.newest.isEnabled = false
@@ -114,17 +112,19 @@ class HomeFragment : Fragment() {
         dialog.btn_save.setOnClickListener {
             storyViewModel.setSection(section)
             storyViewModel.getListStories(section)
-            dialog.dismiss()
-            showDialog = false
+            mBottomSheetDialog.dismiss()
             progressBarHome.visibility = View.VISIBLE
         }
         dialog.btn_cancel.setOnClickListener {
-            showDialog = false
-            dialog.dismiss()
+            mBottomSheetDialog.dismiss()
         }
-        if (!showDialog)
-            dialog.show()
-        showDialog = true
+
+        mBottomSheetDialog.setContentView(dialog)
+        mBottomSheetDialog.show()
+
+        dialog.btnCancelDialog.setOnClickListener{
+            mBottomSheetDialog.dismiss()
+        }
     }
 
     private fun bindings() {
